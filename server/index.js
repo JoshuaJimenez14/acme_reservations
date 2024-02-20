c52cc61865729f8f3a5d82340b31061f3ce7e738
EC2 Default User <ec2-user@ip-172-31-47-72.us-east-2.compute.internal>
2024-02-20 02:55:48 +0000

    i think im done?

--- server/index.js
+++ server/index.js
@@ -8,6 +8,64 @@ const fetchRestaurants = db.fetchRestaurants;
 const createReservation = db.createReservation;
 const fetchReservations = db.fetchReservations;
 const destroyReservation = db.destroyReservation;
+const express = require('express');
+const app = express();
+app.use(express.json());
+
+app.get('/api/customers', async( req, res, next)=> {
+    try {
+        res.send(await fetchCustomers());
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+app.get('/api/restaurants', async( req, res, next)=> {
+    try {
+        res.send(await fetchRestaurants());
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+app.get('/api/customers/:id/reservations', async( req, res, next)=> {
+    try {
+        res.send(await fetchReservations(req.params.id));
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+app.post('/api/customers/:customer_id/reservations', async( req, res, next)=> {
+    try {
+        res.status(201).send(await createReservation({
+            customer_id: req.params.customer_id, 
+            restaurant_id: req.body.restaurant_id,
+            reservation_date: req.body.reservation_date
+            
+        }));
+    }
+    catch(ex){
+        next(ex);
+    }
+});
+
+
+app.delete('/api/customers/:customer_id/reservations/:id', async (req, res, next) => {
+    try {
+        await destroyReservation({ customer_id: req.params.customer_id, id: req.params.id });
+        res.sendStatus(204);
+    } catch (ex) {
+        next(ex);
+    }
+});
+
+app.use((err, req, res, next)=> {
+    res.status(err.status || 500).send({error: err.message || err});
+});
 
 const init = async()=> {
   console.log('connecting to database');  
@@ -26,7 +84,7 @@ const init = async()=> {
      ]);
      console.log(await fetchCustomers());
      console.log(await fetchRestaurants());
-     const reservation = await createReservation({
+     let reservation = await createReservation({
          reservation_date: '04/13/2024',
          restaurant_id:Kura.id,
          customer_id:moe.id
@@ -34,6 +92,21 @@ const init = async()=> {
      console.log( await fetchReservations(moe.id));
      await destroyReservation(reservation);
     console.log( await fetchReservations(moe.id));
+    reservation = await createReservation({
+         reservation_date: '04/19/2024',
+         restaurant_id:Bocchi.id,
+         customer_id:moe.id
+    });
+    const port = process.env.PORT || 3000;
+    app.listen(port, ()=> {
+        console.log(`listening on port ${port}`);
+        console.log(`curl localhost:${port}/api/customers`);
+        console.log(`curl localhost:${port}/api/restaurants`);
+        console.log(`curl localhost:${port}/api/customers/${moe.id}/reservations`);
+        console.log(`curl -X DELETE localhost:${port}/api/customers/${moe.id}/reservations/${reservation.id}`);
+        console.log(`curl localhost:${port}/api/customers/${lucy.id}/reservations`);
+        console.log(`curl -X POST localhost:${port}/api/customers/${lucy.id}/reservations -d '{"restaurant_id":"${Gen.id}","reservation_date":"04/01/2024"}' -H "Content-Type:application/json"-v`);
+    })
 };
 
 init ();
